6928e81230406753aedf4321ef3392ff4135e25e
loopystack19 <sanjaygreg19@gmail.com>
2026-01-18 20:27:41 +0300

    This is my first commit, i have started learning node js

---
+++ app.js
@@ -0,0 +1,6 @@
+const amount=1000;
+let message= amount > 500 ? "The constant amount is greater than 500":"The constant amount is less than 500";
+console.log(message);
+console.log("This is my first node js app");
+
+//Globals-
