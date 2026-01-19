2a9f74659e3e5d8c2dcc81a5a022a451c877154f
loopystack19 <sanjaygreg19@gmail.com>
2026-01-19 14:54:45 +0300

    just implemented the solution to question 1 using async/await

--- Exercise1/exercise1.js
+++ Exercise1/exercise1.js
@@ -8,10 +8,10 @@
 //The Catch: You must use the callback versions (e.g., fs.mkdir, fs.writeFile), not the Sync versions.
 // You’ll experience what developers call "Callback Hell."
 
-const fs=require('fs');
-const path=require('path');
+// const fs=require('fs');
+// const path=require('path');
 
-const folder=path.join(__dirname,'test_folder');
+// const folder=path.join(__dirname,'test_folder');
 const file=path.join(folder, 'hello.txt');
 const newFile=path.join(folder, 'finished.txt');
 
@@ -58,3 +58,43 @@ process.on('uncaughtException', err=>{
     console.log(err);
     process.exit(1);
 })
+
+//Now implementing the soluton to the same problem using async and await
+
+const fsPromises=require('fs').promises;
+const { existsSync } = require('fs');
+const path=require('path');
+
+const runWorkFlow= async ()=>{
+    try{
+
+        const folderPath=path.join(__dirname,'test_folder');
+        const filePath=path.join(folderPath, 'hello.txt');
+        const newFilePath=path.join(folderPath, 'finished.txt');
+
+        //checking and creating the folder
+
+        if(!existsSync(folderPath)){
+            fsPromises.mkdir(folderPath);
+            console.log("Succefully created the test_folder ✅");
+        }
+
+        await fsPromises.writeFile(filePath,"Phase 1");
+        console.log("1.The file has succefully been written ✅");
+
+        await fsPromises.appendFile(filePath,"/nPhase 2");
+        console.log("2.File has been succefully appended ✅");
+
+        await fsPromises.rename(filePath, newFilePath);
+        console.log("The file has been succefully renamed");
+
+        const finalData= await fsPromises.readFile(newFilePath, 'utf8');
+        console.log(finalData);
+
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+runWorkFlow();
