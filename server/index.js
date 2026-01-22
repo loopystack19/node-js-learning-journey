07db148675944290210b4b5ff939a42b150b1bb8
loopystack19 <sanjaygreg19@gmail.com>
2026-01-22 23:27:15 +0300

    i have just learned about http and tcpip protocols and how data moves from the backend to the frontend

---
+++ server/index.js
@@ -0,0 +1,9 @@
+//creating a simple server
+const http=require('http');
+const server=http.createServer((request, response)=>{
+    console.log("We have just received a new request")
+});
+const port=3000;
+server.listen(port,()=>{
+    console.log("The server is running");
+})
