302e1822e3cafd59eb9be87e220ac7bbd28d2d38
loopystack19 <sanjaygreg19@gmail.com>
2026-01-24 15:08:25 +0300

    i have just learned about routing and i have implemented a simple routing programme haha

--- server/index.js
+++ server/index.js
@@ -6,10 +6,25 @@ const path=require('path');
 const fileRead=path.join(__dirname,'templates', 'index.html');
 const data=fs.readFileSync(fileRead,'utf8');
 const server=http.createServer((req, res)=>{
-    res.end(data);
-    console.log("A new Request has been received");
+    const url=req.url;
+    if(url === '/'){
+        res.end(data);
+    }
+    else if( url === '/about'){
+        res.writeHead(200,{"Content-Type":"text/plain"});
+        res.end("This is the about section of the node app");
+    }
+    else if( url === '/contact'){
+        res.writeHead(200,{"Content-Type":"text/plain"});
+        res.end("This is the contact section of the node app");
+    }
+    else{
+        res.writeHead(404,{"Content-Type":"text/plain"});
+        res.end("Sorry we could not locate the page");
+    }
 })
 const port=3000;
 server.listen(port,()=>{
     console.log("The server is running on",port);
 })
+//learning the basics of routing
