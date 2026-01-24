485f42aa0aa0d5da2d79984dde7f5951ace9a08d
loopystack19 <sanjaygreg19@gmail.com>
2026-01-24 15:58:52 +0300

    now i have routed the html page with the actual css styles being implemented

--- server/index.js
+++ server/index.js
@@ -1,37 +1,32 @@
-//here am going to try and send different html resources based on the routes
-const fs= require('fs');
+//lets do the routing thing
+const fs=require('fs');
 const path=require('path');
 const http=require('http');
-//These are the paths to the resources that we need
-let homePage=path.join(__dirname,'Home','index.html');
-let aboutPage=path.join(__dirname,'About','index.html');
-let contactPage=path.join(__dirname,'Contact','index.html');
 
-//lets read from the resources
-let homePageRead=fs.readFileSync(homePage,'utf8');
-let aboutPageRead=fs.readFileSync(aboutPage,'utf8');
-let contactPageRead=fs.readFileSync(contactPage,'utf8');
-
-
-//lets create the server that we need
+const htmlFile=path.join(__dirname,'Home', 'index.html');
+let htmlFileData=fs.readFileSync(htmlFile,'utf8');
 const server=http.createServer((request, response)=>{
-    //here am going to implement the routing logic
-    const url=request.url;
+    const url = request.url;
     if(url === '/'){
-        response.end(homePageRead);
+        response.end(htmlFileData.replace('Hello world ','This is the Home Page'));
     }
     else if(url === '/about'){
-        response.end(aboutPageRead);
+        response.end(htmlFileData.replace('Hello world ','This is the About Page'));
     }
     else if(url === '/contact'){
-        response.end(contactPageRead);
+        response.end(htmlFileData.replace('Hello world ','This is the Contact Page'));
+    }
+    else if(url === '/services'){
+        response.end(htmlFileData.replace('Hello world ','This is the Services Page'))
     }
     else{
-        response.writeHead(404,{"Content-Type":"text/plain"})
-        response.end("Error 404:Page not found");
+        response.writeHead(404,{"Content-Type":"text/plain"});
+        response.end("Error 404:We could not locate the page")
     }
-})
+});
+
 const port=3000;
+
 server.listen(port,()=>{
-    console.log("The server is running at port", port);
+    console.log("The server is running at port:", port);
 })
