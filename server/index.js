6dae714d71daf7a0d9fbadcd82828886b43ff6be
loopystack19 <sanjaygreg19@gmail.com>
2026-01-24 15:28:43 +0300

    we have implemented what we have learned about routing through an exercise

--- server/index.js
+++ server/index.js
@@ -1,30 +1,37 @@
-//lets create a server and send a html response
-const fs=require('fs');
-const http=require('http');
+//here am going to try and send different html resources based on the routes
+const fs= require('fs');
 const path=require('path');
+const http=require('http');
+//These are the paths to the resources that we need
+let homePage=path.join(__dirname,'Home','index.html');
+let aboutPage=path.join(__dirname,'About','index.html');
+let contactPage=path.join(__dirname,'Contact','index.html');
 
-const fileRead=path.join(__dirname,'templates', 'index.html');
-const data=fs.readFileSync(fileRead,'utf8');
-const server=http.createServer((req, res)=>{
-    const url=req.url;
+//lets read from the resources
+let homePageRead=fs.readFileSync(homePage,'utf8');
+let aboutPageRead=fs.readFileSync(aboutPage,'utf8');
+let contactPageRead=fs.readFileSync(contactPage,'utf8');
+
+
+//lets create the server that we need
+const server=http.createServer((request, response)=>{
+    //here am going to implement the routing logic
+    const url=request.url;
     if(url === '/'){
-        res.end(data);
+        response.end(homePageRead);
     }
-    else if( url === '/about'){
-        res.writeHead(200,{"Content-Type":"text/plain"});
-        res.end("This is the about section of the node app");
+    else if(url === '/about'){
+        response.end(aboutPageRead);
     }
-    else if( url === '/contact'){
-        res.writeHead(200,{"Content-Type":"text/plain"});
-        res.end("This is the contact section of the node app");
+    else if(url === '/contact'){
+        response.end(contactPageRead);
     }
     else{
-        res.writeHead(404,{"Content-Type":"text/plain"});
-        res.end("Sorry we could not locate the page");
+        response.writeHead(404,{"Content-Type":"text/plain"})
+        response.end("Error 404:Page not found");
     }
 })
 const port=3000;
 server.listen(port,()=>{
-    console.log("The server is running on",port);
+    console.log("The server is running at port", port);
 })
-//learning the basics of routing
