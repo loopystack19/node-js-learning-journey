ebac4bdf565e8fa493ea4d0b7aec3fd094d89d9d
loopystack19 <sanjaygreg19@gmail.com>
2026-01-24 14:49:13 +0300

    we just sent our first html response

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
-//creating a simple server
+//lets create a server and send a html response
+const fs=require('fs');
 const http=require('http');
-const server=http.createServer((request, response)=>{
-    console.log("We have just received a new request")
-});
+const path=require('path');
+
+const fileRead=path.join(__dirname,'templates', 'index.html');
+const data=fs.readFileSync(fileRead,'utf8');
+const server=http.createServer((req, res)=>{
+    res.end(data);
+    console.log("A new Request has been received");
+})
 const port=3000;
 server.listen(port,()=>{
-    console.log("The server is running");
+    console.log("The server is running on",port);
 })
